5aae2444c9f8e0c83e004e3f6368e64d6a3cc8b5
Escalona1 <lucas@DESKTOP-4UK13F7>
2024-05-08 09:22:28 -0400

    implementación frontend crear rutina y evaluacion

--- src/controllers/evaluacionController.js
+++ src/controllers/evaluacionController.js
@@ -2,7 +2,7 @@ import * as evaluacionService from '../services/evaluacionService.js';
 
 export const createEvalucion = async (req, res) => {
     try {
-        const evaluacion = await evaluacionService.createEvalucion(req.body.id_evaluacion, req.body.peso, req.body.estatura, req.body.grasa, req.body.masa_muscular, req.body.agua, req.body.masa_osea, req.body.agua, req.body.edad, req.body.experencia, req.body.id_entrenador, req.body.fecha_evaluacion, req.body.id_cliente)
+        const evaluacion = await evaluacionService.createEvalucion(req.body.id_evaluacion, req.body.peso, req.body.estatura, req.body.grasa, req.body.masa_muscular, req.body.agua, req.body.masa_osea, req.body.edad, req.body.experencia, req.body.id_entrenador, req.body.fecha_evaluacion, req.body.id_cliente)
         res.status(200).json(evaluacion);
     } catch (error) {
         console.error('Error', error);
