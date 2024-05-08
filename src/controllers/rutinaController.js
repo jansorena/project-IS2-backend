7d05fceb6afff058f26c93a3b0589efc1c4df750
Escalona1 <lucas@DESKTOP-4UK13F7>
2024-05-08 14:38:22 -0400

    vistas crear evaluacion y rutina

--- src/controllers/rutinaController.js
+++ src/controllers/rutinaController.js
@@ -3,7 +3,6 @@ import * as rutinaService from '../services/rutinaService.js';
 export const createRutina = async (req, res) => {
     try {
         const {id_rutina, clasificacion, id_cliente, id_entrenador, fecha_rutina, ejercicios} = req.body;
-        console.log(req)
         const rutina = await rutinaService.createRutina(id_rutina, clasificacion, id_cliente, id_entrenador, fecha_rutina, ejercicios);
         res.status(200).json(rutina);
     } catch (error) {
--- src/services/rutinaService.js
+++ src/services/rutinaService.js
@@ -1,6 +1,5 @@
 import Rutina from '../models/rutinaModel.js';
 
 export async function createRutina(id_rutina,clasificacion, id_cliente, id_entrenador, fecha_rutina, ejercicios){
-    console.log(id_rutina);
     return await Rutina.create(id_rutina,clasificacion, id_cliente, id_entrenador, fecha_rutina, ejercicios)
 }
