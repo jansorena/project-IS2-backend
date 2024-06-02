ce4f4840f16a0ab2059196004e2fba637e8d5cdc
DayanSC <dasaez2020@inf.udec.cl>
2024-06-02 16:39:04 -0400

    Finalización funcion createRutina

--- src/models/rutinaModel.js
+++ src/models/rutinaModel.js
@@ -1,12 +1,15 @@
 import db from "../config/db.js";
 
 class Rutina {
-    static async create(id_rutina, clasificacion, id_cliente, id_entrenador, fecha_rutina, circuitos, ejercicios){
+    static async create(id_rutina, clasificacion, id_cliente, id_usuario, fecha_rutina, circuitos){
         let queryCircuito = 'INSERT INTO circuito (id_circuito, repeticiones) VALUES ';
         let queryContiene = 'INSERT INTO contiene (id_rutina, id_circuito, descanso) VALUES ';
         circuitos.forEach((circuito,index) => {
-            queryCircuito += `(${circuito[0]}, ${circuito[1]})`;
-            queryContiene += `(${id_rutina}, ${circuito[0]}, ${circuito[2]})`;
+            queryCircuito += `(${circuito.id_circuito}, ${circuito.repeticiones})`;
+            if(circuito.descanso)
+                queryContiene += `(${id_rutina}, ${circuito.id_circuito}, '${circuito.descanso}')`;
+            else
+                queryContiene += `(${id_rutina}, ${circuito.id_circuito}, "N/A")`;
             if (index < circuitos.length - 1) {
                 queryCircuito += ', ';
                 queryContiene += ', ';
@@ -15,17 +18,20 @@ class Rutina {
         
         let queryCompone = 'INSERT INTO compone (id_circuito, id_ejercicio, series, frecuencia, orden, descanso) VALUES ';
         circuitos.forEach((circuito, index) => {
-            ejercicios[index].forEach((ejercicio) => {
-            queryCompone += `(${circuito[0]}, ${ejercicio[0]}, ${ejercicio[1]}, ${ejercicio[2]}, ${ejercicio[3]}, ${ejercicio[4]})`;
+            circuito.ejercicios.forEach((ejercicio, ejercicioIndex) => {
+                queryCompone += `(${circuito.id_circuito}, ${ejercicio.id_ejercicio}, ${ejercicio.series}, '${ejercicio.frecuencia}', ${ejercicio.orden}, '${ejercicio.descan}')`;
+                if (ejercicioIndex < circuito.ejercicios.length - 1) {
+                    queryCompone += ', ';
+                }
+            });
             if (index < circuitos.length - 1) {
                 queryCompone += ', ';
             }
-            });
         });
 
         const result = await db.batch([
             `INSERT INTO rutina (id_rutina, clasificacion) VALUES (${id_rutina}, '${clasificacion}')`,
-            `INSERT INTO crea (id_entrenador, id_rutina, fecha_rutina) VALUES (${id_entrenador}, ${id_rutina}, '${fecha_rutina}')`,
+            `INSERT INTO crea (id_entrenador, id_rutina, fecha_rutina) VALUES (${id_usuario}, ${id_rutina}, '${fecha_rutina}')`,
             `INSERT INTO tiene (id_rutina, id_cliente) VALUES (${id_rutina} ,${id_cliente})`,
             queryCircuito,
             queryContiene,
