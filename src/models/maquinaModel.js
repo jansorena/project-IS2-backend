e5b5498741ccdd5711d777a7323ad7c79e0ae107
Escalona1 <lucas@DESKTOP-5ESDPBN>
2024-06-26 19:16:37 -0400

    modificacion getAllMaquinas()

--- src/models/maquinaModel.js
+++ src/models/maquinaModel.js
@@ -10,46 +10,18 @@ class Maquina {
                     m.id_maquina,
                     m.nombre AS nombre_maquina,
                     m.estado,
-                    e.nombre AS nombre_ejercicio
-                    FROM
-                        maquina m
-                    LEFT JOIN
-                        utiliza u ON m.id_maquina = u.id_maquina
-                    LEFT JOIN
-                        ejercicio e ON u.id_ejercicio = e.id_ejercicio;
+                    m.descripcion
+                    m.reporte
                     `,
                     args: [],
                 });
                 console.log('Result:', result);
                 if(!result || !result.rows) {
-                    console.error('No se encontraron filas en la consulta');
+                    console.error('No se encontraron filas en la consultas');
                     return [];
                 }
-                const maquinas = {};
-
-                result.rows.forEach(row => {
-                    const { id_maquina, nombre_maquina, estado, id_ejercicio, nombre_ejercicio } = row;
-                    
-                    if (!maquinas[id_maquina]) {
-                    maquinas[id_maquina] = {
-                        id_maquina,
-                        nombre_maquina,
-                        estado,
-                        ejercicios: []
-                    };
-                    
-                    }
-
-                    if (row.nombre_ejercicio) {
-                    maquinas[id_maquina].ejercicios.push({
-                        id_ejercicio,
-                        nombre_ejercicio
-     
-                    });
-                    }
-                });
-
-                return Object.values(maquinas);
+  
+                return result;
 
         } catch (error) {
             console.error('Error ejecutando getAllmaquinas:', error);
